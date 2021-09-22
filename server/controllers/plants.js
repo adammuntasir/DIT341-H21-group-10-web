b50869306ba4e5726a65ffd5a4b2cd15adbe45dd
Muntasir Adam <gusadammu@student.gu.se>
2021-09-22 22:57:48 +0200

    Add sorting to get plants endpoint.

--- server/controllers/plants.js
+++ server/controllers/plants.js
@@ -1,119 +1,128 @@
-var express = require('express');
-const { Schema } = require('mongoose');
+var express = require("express");
+const { Schema } = require("mongoose");
 var router = express.Router();
 
-var Plant = require('../models/plant');
+var Plant = require("../models/plant");
 
 // (a) POST /plants
-router.post('/api/plants', function (req, res, next) {
-    var plant = new Plant(req.body);
-    plant.save(function (err, plant) {
-        if (err) {
-            return next(err);
-        }
-        else res.status(201).json(plant);
-
-    });
-
+router.post("/api/plants", function (req, res, next) {
+  var plant = new Plant(req.body);
+  plant.save(function (err, plant) {
+    if (err) {
+      return next(err);
+    } else res.status(201).json(plant);
+  });
 });
 
 // (b) GET /plants
 
-router.get('/api/plants', function (req, res, next) {
-    console.log(req.query)
-    Plant.find(function (err, plants) {
-        if (err) {
+router.get("/api/plants", function (req, res, next) {
+  console.log(req.query);
+  var query = req.query.season;
+  if (query) {
+    if (
+      query === "desc" ||
+      query === "asc" ||
+      query === "1" ||
+      query === "-1"
+    ) {
+      Plant.find({})
+        .sort({ season: query })
+        .exec(function (err, plants) {
+          if (err) {
             return next(err);
-        }
-        res.status(200).json({ 'plants': plants });
-
-    }).sort(req.query);
-
+          }
+          res.status(200).json({ plants: plants });
+        });
+    } else {
+      next({ message: "wrong query parameters" });
+    }
+  } else {
+    Plant.find(function (err, plants) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json({ plants: plants });
+    });
+  }
 });
 
 // (c) DELETE /plants
-router.delete('/api/plants', function (req, res, next) {
-    Plant.remove({}, function (err) {
-        if (err) {
-            return next(err);
-        } 
-        else res.status(200).json({ "message": 'Deleted!' })
-    })
+router.delete("/api/plants", function (req, res, next) {
+  Plant.remove({}, function (err) {
+    if (err) {
+      return next(err);
+    } else res.status(200).json({ message: "Deleted!" });
+  });
 });
 
-
 // (d) GET /plants/:id
-router.get('/api/plants/:id', function (req, res, next) {
-    var id = req.params.id;
-    Plant.findById(req.params.id, function (err, plant) {
-        if (err) {
-            return next(err);
-        }
-        if (plant == null) {
-            return res.status(404).json(
-                { "message": "Plant not found" });
-        }
-        res.status(200).json(plant);
-    });
+router.get("/api/plants/:id", function (req, res, next) {
+  var id = req.params.id;
+  Plant.findById(req.params.id, function (err, plant) {
+    if (err) {
+      return next(err);
+    }
+    if (plant == null) {
+      return res.status(404).json({ message: "Plant not found" });
+    }
+    res.status(200).json(plant);
+  });
 });
 
 // (e) PUT /plants/:id
 
-router.put('/api/plants/:id', function (req, res, next) {
-    var id = req.params.id;
-    Plant.findById(id, function (err, plant) {
-        if (err) {
-            return next(err);
-        }
-        if (plant == null) {
-            return res.status(404).json(
-                { "message": "Plant not found" });
-        }
-        plant.name = req.body.name;
-        plant.type = req.body.type;
-        plant.color = req.body.color;
-        plant.season = req.body.season;
-        plant.price = req.body.price;
-        plant.save();
-        res.status(200).json(plant);
-    });
+router.put("/api/plants/:id", function (req, res, next) {
+  var id = req.params.id;
+  Plant.findById(id, function (err, plant) {
+    if (err) {
+      return next(err);
+    }
+    if (plant == null) {
+      return res.status(404).json({ message: "Plant not found" });
+    }
+    plant.name = req.body.name;
+    plant.type = req.body.type;
+    plant.color = req.body.color;
+    plant.season = req.body.season;
+    plant.price = req.body.price;
+    plant.save();
+    res.status(200).json(plant);
+  });
 });
 
-
 // (f) PATCH /plants/:id
-router.patch('/api/plants/:id', function (req, res, next) {
-    var id = req.params.id;
-    Plant.findById(id, function (err, plant) {
-        if (err) {
-            return next(err);
-        }
-        if (plant == null) {
-            return res.status(404).json(
-                { "message": "Plant not found" });
-        }
-        plant.name = (req.body.name || plant.name);
-        plant.type = (req.body.type || plant.type);
-        plant.color = (req.body.color || plant.color);
-        plant.season = (req.body.season || plant.season);
-        plant.price = (req.body.price || plant.price);
-        plant.save();
-        res.status(200).json(plant);
-    });
+router.patch("/api/plants/:id", function (req, res, next) {
+  var id = req.params.id;
+  Plant.findById(id, function (err, plant) {
+    if (err) {
+      return next(err);
+    }
+    if (plant == null) {
+      return res.status(404).json({ message: "Plant not found" });
+    }
+    plant.name = req.body.name || plant.name;
+    plant.type = req.body.type || plant.type;
+    plant.color = req.body.color || plant.color;
+    plant.season = req.body.season || plant.season;
+    plant.price = req.body.price || plant.price;
+    plant.save();
+    res.status(200).json(plant);
+  });
 });
 
 // (g) DELETE /plants/:id
-router.delete('/api/plants/:id', function (req, res, next) {
-    var id = req.params.id;
-    Plant.findOneAndDelete({ _id: id }, function (err, plant) {
-        if (err) {
-            return next(err);
-        }
-        if (plant == null) {
-            return res.status(404).json(
-                { "message": "Plant not found" });
-        }
-        res.status(200).json(plant);
-    });
+router.delete("/api/plants/:id", function (req, res, next) {
+  var id = req.params.id;
+  Plant.findOneAndDelete({ _id: id }, function (err, plant) {
+    if (err) {
+      return next(err);
+    }
+    if (plant == null) {
+      return res.status(404).json({ message: "Plant not found" });
+    }
+    res.status(200).json(plant);
+  });
 });
 
 module.exports = router;

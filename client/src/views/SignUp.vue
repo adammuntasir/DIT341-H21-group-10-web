189f080392287e50ea3a5770f534f1eb2d2dfb1f
Aleksandar Isakovski <aleisa@net.chalmers.se>
2021-10-05 16:59:19 +0200

    added input value fields to sighnup to further insert in the database

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
         <h1>I'm In the signUp</h1>
+        <p>email adress</p>
+        <input v-model="email" placeholder="ex. 1234@yahoo.com">
+        <p>user name</p>
+        <input v-model="username" placeholder="username">
+        <p>password</p>
+        <input v-model="password" placeholder="password">
+        <p>confirm password</p>
+        <input v-model="passwordConf" placeholder="password">
+        <p>Are you a gardener or a costumer</p>
+        <select v-model="selected">
+          <option disabled value="">Please select one</option>
+          <option>Costumer</option>
+          <option>Gardener</option>
+        </select>
     </div>
 </template>
+
+<script>
+// import { Api } from '@/Api'
+export default {
+  data() {
+    return {
+      data: {
+        selected: 'costumer',
+        options: [{ text: 'costumer', value: 'costumer' }, { text: 'gardener', value: 'gardener' }]
+      }
+    }
+  }
+}
+</script>

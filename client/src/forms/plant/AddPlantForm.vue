896a8c8ba8454e93d9189593d1c67367b424c92c
Muntasir Adam <gusadammu@student.gu.se>
2021-10-12 22:45:31 +0200

    Add forms to add plants

---
+++ client/src/forms/plant/AddPlantForm.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Name:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.Name"
+          type="name"
+          placeholder="Plant Name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Type:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.type"
+          placeholder="Enter type"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="color:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.color"
+          placeholder="Enter color"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="season:" label-for="input-4">
+        <b-form-select
+          id="input-4"
+          v-model="form.season"
+          :options="season"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-5" label="price:" label-for="input-5">
+        <b-form-input
+          id="input-5"
+          v-model="form.price"
+          placeholder="Enter price"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+        <b-form-checkbox-group
+          v-model="form.checked"
+          id="checkboxes-4"
+          :aria-describedby="ariaDescribedby"
+        >
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import { Api } from '@/Api'
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        type: '',
+        color: '',
+        season: null,
+        price: 0,
+        checked: []
+      },
+      season: [
+        { text: 'Select season', value: null },
+        'Summer',
+        'Autumn',
+        'Winter',
+        'Spring'
+      ],
+      show: true
+    }
+  },
+  methods: {
+    addPlant(name, type, color, season, price) {
+      var newPlant = {
+        name: this.form.name,
+        type: this.form.type,
+        color: this.form.color,
+        season: this.form.season,
+        price: this.form.price
+      }
+      Api.post('/plants/', newPlant)
+        .then(response => {
+          this.plants.push(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+          //  to do dispaly some error message istead of logging to consle
+        })
+        .then(() => {
+          console.log('this run every time after sucess or error.')
+        })
+    },
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+      this.addPlant()
+    },
+    onReset(event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
--- client/src/views/Plants.vue
+++ client/src/views/Plants.vue
@@ -1,48 +1,76 @@
 <template>
-<div>
-<div><button type="button" v-on:click="addPlant">Add a plant!</button></div>
-<div><button type="button" v-on:click="deletePlants">delete all plants!</button></div>
+  <div>
+    <div><button type="button" v-on:click="addPlant">Add a plant!</button></div>
+    <div>
+      <button type="button" v-on:click="deletePlants">
+        delete all plants!
+      </button>
+    </div>
 
-  <b-container fluid="md" class="myContainer">
-    <!-- <b-row>
+    <b-container fluid="md" class="myContainer">
+      <div>
+      <div class="sign-up-page add-white-space">
+        <b-card class="sign-up-form-container">
+          <b-card-body class="mx-2">
+            <h1 class="mt-2 mb-5">
+              Add Plant!
+            </h1>
+
+            <add-plant-form />
+
+            <p class="mt-2">
+              Already registered?
+              <router-link to="/sign-in">Sign in here</router-link>
+            </p>
+          </b-card-body>
+        </b-card>
+      </div>
+    </div>
+      <!-- <b-row>
         <b-col cols="3" offset="1" offset-md="2">
            <b-form-input v-model="text" placeholder="Which plant do you need"></b-form-input>
         </b-col>
       </b-row> -->
-    <b-row>
-      <b-col
-        v-for="plant in plants"
-        v-bind:key="plant._id"
-        cols="12"
-        sm="6"
-        md="4"
-      >
-        <plant-item v-bind:plant="plant" v-on:del-plant="deletePlant" />
-         <div><button type="button" v-on:click="getPlant(plant._id)">get a plant id!</button></div>
-      </b-col>
-    </b-row>
-  </b-container>
+      <b-row>
+        <b-col
+          v-for="plant in plants"
+          v-bind:key="plant._id"
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <plant-item v-bind:plant="plant" v-on:del-plant="deletePlant" />
+          <div>
+            <button type="button" v-on:click="getPlant(plant._id)">
+              get a plant id!
+            </button>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import PlantItem from '../components/PlantItem.vue'
+import AddPlantForm from '../forms/plant/AddPlantForm.vue'
 import { Api } from '@/Api'
 
 export default {
   name: 'plants',
   components: {
-    'plant-item': PlantItem
+    'plant-item': PlantItem,
+    'add-plant-form': AddPlantForm
   },
   mounted() {
     console.log('Page is loaded!')
     // load the plant from server
     Api.get('/plants')
-      .then((response) => {
+      .then(response => {
         console.log(response)
         this.plants = response.data.plants
       })
-      .catch((error) => {
+      .catch(error => {
         this.plants = []
         console.log(error)
         //  to do dispaly some error message istead of logging to consle
@@ -54,8 +82,8 @@ export default {
   methods: {
     deletePlant(id) {
       console.log(`Delete plant with id ${id}`)
-      Api.delete(`/plants/${id}`).then((response) => {
-        const index = this.plants.findIndex((plant) => plant._id === id)
+      Api.delete(`/plants/${id}`).then(response => {
+        const index = this.plants.findIndex(plant => plant._id === id)
         this.plants.splice(index, 1)
       })
       // TODO: catch error
@@ -70,10 +98,10 @@ export default {
         price: this.price
       }
       Api.post('/plants/', newPlant)
-        .then((response) => {
+        .then(response => {
           this.plants.push(response.data)
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error)
           //  to do dispaly some error message istead of logging to consle
         })
@@ -91,12 +119,12 @@ export default {
         .catch(error => {
           this.plants = []
           console.log(error)
-        //  to do dispaly some error message istead of logging to consle
+          //  to do dispaly some error message istead of logging to consle
         })
         .then(() => {
           console.log('this run every time after sucess or error.')
         })
-        // TODO: catch error
+      // TODO: catch error
     },
     getPlant(id) {
       console.log(`get plant with id ${id}`)
